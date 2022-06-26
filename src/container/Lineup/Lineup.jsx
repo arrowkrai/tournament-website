ffa2f1ac403e60890343f32d978dcba0ccf8608a
arrowkrai <arrowkrai@gmail.com>
2022-06-26 13:51:25 -0700

    Add 3 Smash Competitors

--- src/container/Lineup/Lineup.jsx
+++ src/container/Lineup/Lineup.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { FiArrowLeft, FiArrowRight } from "react-icons/fi";
 import { FaTwitter } from "react-icons/fa";
 import Slider from "react-slick";
-import ab1 from "../../assets/ab1.jpeg";
+import mkleo from "../../assets/mkleo.jpg";
+import zain from '../../assets/zain.jpg';
+import shuton from '../../assets/shuton.jpg'
 import "./Lineup.scss";
 import { motion } from "framer-motion";
 
@@ -22,6 +24,27 @@ function PrevArrow(props) {
   );
 }
 
+const smashPlayers = [
+  {
+    picture: mkleo,
+    name: "MkLeo",
+    description: "MkLeo, also known simply as Leo, is a smasher from Mexico. He is widely considered to be the best Super Smash Bros. Ultimate player in the world, dominating the competitive scene since the game's release. He is currently ranked 1st on the Mexican Ultimate Power Rankings, OrionRank Ultimate: Eclipse North America, OrionRank Ultimate: Eclipse, and PGRUContenders North America.",
+    twitter: "https://twitter.com/Mkleosb",
+  },
+  {
+    picture: zain,
+    name: "Zain",
+    description: "Zain, also known as DontTestMe is a Melee Marth main from Reston, Virginia and is considered to be one of the best Melee players in the world. He is known for his incredibly lethal punish game on space animal players and methodical play against floaty characters, and is currently considered to be the best Marth player in the world.",
+    twitter: "https://twitter.com/ZainNaghmi",
+  },
+  {
+    picture: shuton,
+    name: "Shuton",
+    description: "Shuton (しゅーとん) is a Super Smash Bros. Ultimate Pyra, Mythra, and Olimar player from Fukuoka, Japan. He is considered the best Pyra and Mythra player in Japan and one of the two best Olimar players in the world, along with Dabuz. With his consistently strong performances both in Japan and abroad, he is considered one of the best players in Japan.",
+    twitter: "https://twitter.com/syu_tolimar",
+  },
+];
+
 const Lineup = () => {
   const settings = {
     // dots: true,
@@ -40,7 +63,21 @@ const Lineup = () => {
           <h1 className="app__lineup-header">Meet Your Competitors</h1>
         </motion.div>
         <Slider {...settings}>
-        <div className="slider-item">
+          {smashPlayers.map((player) => (
+            <div className="slider-item" key={player.name}>
+              <div className="slider-item-content">
+                <img src={player.picture} alt={player.name} />
+                <div className="slider-item-text">
+                  <h1>{player.name}</h1>
+                  <p>{player.description}</p>
+                  <a href={player.twitter}>
+                    <FaTwitter />
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+          {/* <div className="slider-item">
             <div className="slider-item-content">
               <img src={ab1} alt="" />
               <div className="slider-item-text">
@@ -51,31 +88,7 @@ const Lineup = () => {
                 </a>
               </div>
             </div>
-          </div>
-          <div className="slider-item">
-            <div className="slider-item-content">
-              <img src={ab1} alt="" />
-              <div className="slider-item-text">
-                <h1>MkLeo</h1>
-                <p>MkLeo is known as of the best smash players</p>
-                <a href="">
-                  <FaTwitter />
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="slider-item">
-            <div className="slider-item-content">
-              <img src={ab1} alt="" />
-              <div className="slider-item-text">
-                <h1>MkLeo</h1>
-                <p>MkLeo is known as of the best smash players</p>
-                <a href="">
-                  <FaTwitter />
-                </a>
-              </div>
-            </div>
-          </div>
+          </div> */}
         </Slider>
       </div>
     </div>
